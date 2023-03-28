41b80148b217512651c0f7c06d2d011d80aac0bf
mrouw <bchva@yandex.ru>
2023-03-28 11:14:20 +0700

    defined the constructors

--- classinf.cpp
+++ classinf.cpp
@@ -1,6 +1,25 @@
 #include "classinf.h"
+#include <QString>
+#include <QFileInfo>
 
-ClassInf::ClassInf()
+ClassInf::ClassInf(const QString& fName)
 {
+    f_name = fName;
+    QFileInfo file (f_name); // создаем объект и определяем его параметры
+    f_size = file.exists();
+    f_exist = file.exists();
+}
 
+bool ClassInf::is_f_exist() // определяем существование
+{
+    return f_exist;
+}
+int ClassInf::get_f_size() // определяем размер файла
+{
+    return f_size;
+}
+
+QString ClassInf::get_f_name() // определяем имя файла
+{
+    return f_name;
 }
