63363096e0d17abbe7b50e9657c1ded15c316c01
mrouw <bchva@yandex.ru>
2023-04-28 02:44:27 +0700

    description of main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <classinf.h>
+#include <filemonitor.h>
+#include <fileprinter.h>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    setlocale(LC_ALL, "Russian");
+    //FileMonitor& mon; // создаем объект класса
+
+    mon.addfile("C:/Qt/projects/laboratornaya1/f1.txt"); // добавляем файл
+    //mon.delfile("C:/Qt/projects/laboratornaya1/f1.txt"); // удаляем файл
+
 
     return a.exec();
 }
