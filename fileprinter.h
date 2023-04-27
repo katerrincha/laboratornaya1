52bb5fe30e8471223633560480d6ff55a0d68b9d
mrouw <bchva@yandex.ru>
2023-04-27 23:15:47 +0700

    the fileprinter class is written

--- fileprinter.h
+++ fileprinter.h
@@ -7,6 +7,11 @@ class fileprinter
 {
 public:
     fileprinter();
+
+public slots:
+    void printexists(QString f_name, int f_size); // вывод при создании файла
+    void printdeleted(QString f_name); // вывод при удалении файла
+    void printchanged(QString f_name, int f_size); // вывод при измнении файла
 };
 
 #endif // FILEPRINTER_H
