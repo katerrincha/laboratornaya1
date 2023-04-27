3712d7946b12dd8f1fcbe24604806aa32b712fea
mrouw <bchva@yandex.ru>
2023-04-27 23:27:02 +0700

    fileprinter description

--- fileprinter.cpp
+++ fileprinter.cpp
@@ -1,6 +1,20 @@
 #include "fileprinter.h"
+#include <iostream>
+using namespace std;
 
 fileprinter::fileprinter()
-{
+{    
+}
 
+void fileprinter::printexists(QString f_name, int f_size) // вывод при создании файла
+{
+    cout << "Создан файл: "<<  f_name.toStdString() << ", размер: " << f_size << endl;
+}
+void fileprinter::printdeleted(QString f_name) // вывод при удалении файла
+{
+    cout << "Удалён файл: "<<  f_name.toStdString() << endl;
+}
+void fileprinter::printchanged(QString f_name, int f_size) // вывод при изменении размера файла
+{
+    cout << "Изменён размер файла: "<<  f_name.toStdString() << ", новый размер: " << f_size << endl;
 }
